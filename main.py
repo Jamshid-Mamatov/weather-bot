6bd245d7688a71c1d75dd2a56940096ce5856ec2
Jamshid <mamatovjamshid506@gmail.com>
2021-02-04 22:44:15 +0500

    send message and update id

--- main.py
+++ main.py
@@ -4,15 +4,28 @@ from pprint import pprint
 import requests
 token=os.environ['token']
 
-api_key=
-url=f"http://api.openweathermap.org/data/2.5/forecast"
-#http://api.openweathermap.org/data/2.5/forecast?q=jizzax&appid=f9716fd07196bb4a858f3d8cddfec94d
-def getUpdate():
-    url=f'https://api.telegram.org/bot{token}/getUpdate'
+
+
+def getUpdate_text():
+    url=f'https://api.telegram.org/bot{token}/getUpdates'
     r=requests.get(url)
     data=r.json()['result']
+    
     text=data[-1]['message']['text']
-    weather(text)
+    if text=='/start':
+        text='*shaxaringizni kiriting*'
+        sendMsg(text)
+    else:
+        weather(text)
+
+def getUpdate_id():
+    url=f'https://api.telegram.org/bot{token}/getUpdates'
+    r=requests.get(url)
+
+    data=r.json()['result']
+    chat_id=data['message']['chat']['id']
+    return chat_id
+
 
 def weather(text):
     api_key='f9716fd07196bb4a858f3d8cddfec94d'
@@ -23,7 +36,30 @@ def weather(text):
     }
     r=requests.get(url,params=payload)
 
+    data=r.json()
+    if r.status_code==200:
+
+        text=text.title()
+        inform=data['list'][-1]
+        temp=inform['main']['temp']-273.15
+        description=inform['weather'][0]['description']
+        wind=inform['wind']['speed']
+        weather=f"from:*{text}*\nTemp:{temp}\ndescription:{description}\nwind:{wind}m/s"
+        sendMsg(weather)
+    else:
+        weather='city not found'
+        sendMsg(weather)
 
 
-print(requests.get(url))
+def sendMsg(text):
+    url=f'https://api.telegram.org/bot{token}/sendMessage'
+    chat_id=getUpdate_id()
+    payload={
+        'chat_id':chat_id,
+        'text':text,
+        'parse_mode':'MarkdownV2 '
+
+    }
+    r=requests.get(url)
+
 
