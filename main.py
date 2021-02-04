17dadc8bd71d9c2b508d4df6824d9c57f2b42426
Jamshid <mamatovjamshid506@gmail.com>
2021-02-04 21:41:20 +0500

    weather function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import requests
 token=os.environ['token']
 
-api_key='ed5e5685450cdf033dff479d927e13f2'
+api_key=
 url=f"http://api.openweathermap.org/data/2.5/forecast"
 #http://api.openweathermap.org/data/2.5/forecast?q=jizzax&appid=f9716fd07196bb4a858f3d8cddfec94d
 def getUpdate():
@@ -12,7 +12,16 @@ def getUpdate():
     r=requests.get(url)
     data=r.json()['result']
     text=data[-1]['message']['text']
-    
+    weather(text)
+
+def weather(text):
+    api_key='f9716fd07196bb4a858f3d8cddfec94d'
+    url=f'http://api.openweathermap.org/data/2.5/forecast'
+    payload={
+        'q':text,
+        'appid':api_key
+    }
+    r=requests.get(url,params=payload)
 
 
 
