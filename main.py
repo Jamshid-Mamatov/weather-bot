7e48d10fb047757a3a59f9a921f52ed4f86e86ab
Jamshid <mamatovjamshid506@gmail.com>
2021-02-04 23:14:47 +0500

    add while weather bot is working

--- main.py
+++ main.py
@@ -12,24 +12,23 @@ def getUpdate_text():
     data=r.json()['result']
     
     text=data[-1]['message']['text']
-    if text=='/start':
-        text='*shaxaringizni kiriting*'
-        sendMsg(text)
-    else:
-        weather(text)
+    
+    return text
 
-def getUpdate_id():
+
+def getchat_id():
     url=f'https://api.telegram.org/bot{token}/getUpdates'
     r=requests.get(url)
 
     data=r.json()['result']
-    chat_id=data['message']['chat']['id']
+    chat_id=data[-1]['message']['chat']['id']
     return chat_id
 
 
-def weather(text):
+def weather():
     api_key='f9716fd07196bb4a858f3d8cddfec94d'
     url=f'http://api.openweathermap.org/data/2.5/forecast'
+    text=getUpdate_text()
     payload={
         'q':text,
         'appid':api_key
@@ -41,25 +40,47 @@ def weather(text):
 
         text=text.title()
         inform=data['list'][-1]
-        temp=inform['main']['temp']-273.15
+        temp=int(inform['main']['temp']-273.15)
         description=inform['weather'][0]['description']
         wind=inform['wind']['speed']
-        weather=f"from:*{text}*\nTemp:{temp}\ndescription:{description}\nwind:{wind}m/s"
-        sendMsg(weather)
+        weather=f"from:{text}\nTemp:{temp}\ndescription:{description}\nwind:{wind}m/s"   
     else:
         weather='city not found'
-        sendMsg(weather)
+    
+
+    return weather
 
 
-def sendMsg(text):
+def sendMsg():
     url=f'https://api.telegram.org/bot{token}/sendMessage'
-    chat_id=getUpdate_id()
+    chat_id=getchat_id()
+    text=weather()
+    print(text)
     payload={
         'chat_id':chat_id,
         'text':text,
-        'parse_mode':'MarkdownV2 '
+        
 
     }
+    r=requests.get(url,payload)
+
+def updateId():
+    url=f'https://api.telegram.org/bot{token}/getUpdates'
     r=requests.get(url)
 
+    data=r.json()['result']
+    update_id=data[-1]['update_id']
+    return update_id
 
+
+last_update_id=None
+while True:
+    new_update_id=updateId()
+    if last_update_id!=new_update_id:
+        sendMsg()
+        print(0)
+        last_update_id=new_update_id
+        
+
+
+    
